5d53a2c223637f0dac5752ba6b82d677759057ce
Nora Coates <noracoates@students.berkeley.net>
2018-01-22 11:35:32 -0800

    hi

---
+++ interface.js
@@ -0,0 +1,18 @@
+function setSelect() {
+    var returnVal = "";
+    for (var i = 0; i < 25; i++) {
+        returnVal += "<option id = '"+ (i+1).toString() + "'>" + (i+1).toString() + "</option>";
+    }
+    document.getElementById("numResults").innerHTML += returnVal;
+}
+
+function display(rows, artist) {
+    //rows = number of results the user selected from dropdown
+    //artist = validateName(NAME);
+    var table = "<table id = 'displaytable" + artist + "'>";
+    for (var i = 0; i < rows; i++) {
+        //iterate thru artists' itunes library
+    }
+    table += "</table>";
+    document.getElementById("display").innerHTML += table;
+}
---
+++ itunes_api.js
@@ -0,0 +1,32 @@
+
+
+/*
+$.ajax({
+   url: “https://itunes.apple.com/search?term=jack+johnson”,
+   type: 'GET',
+   crossDomain: true,
+   dataType: 'jsonp',
+   success: function(result) {
+console.log(result);
+myFunction(result) },
+   error: function() { alert('Failed!'); }
+});
+
+ */
+
+var NAME = '';
+
+function validateName() {
+    NAME = document.getElementById("artistName").value;
+    var length = NAME.length;
+    for(var i = 0; i < length; i++) {
+        if(NAME[i] === " ") {
+            NAME = NAME.split(0,i) + "+" NAME.split(i+1, NAME.length);
+        }
+    }
+    console.log(NAME);
+}
+
+function search() {
+    //jquery
+}
