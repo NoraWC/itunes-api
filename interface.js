27294332bd0d8f04389a8d528dd084963af87fcc
Nora Coates <noracoates@students.berkeley.net>
2018-01-30 10:34:52 -0800

    hi

--- interface.js
+++ interface.js
@@ -1,35 +1,3 @@
-var go;
-/*
-function validateName(x) {
-    var length = x.length;
-    for(var i = 0; i < length; i++) {
-        if(x[i] === " ") {
-            var y = x.substring(0,i);
-            y = y + "+";
-            x = y + x.substring(i+1, x.length);
-        }
-    }
-}
-*/
-function buttons () {
-    document.getElementById('allbuttons').innerHTML = "<button id = 'search' onclick = 'display(go);";
-    /*
-    document.getElementById('allbuttons').innerHTML +=
-        $.ajax({
-            url: 'https://itunes.apple.com/search?term=' + document.getElementById('artistName').value,
-            type: 'GET',
-            crossDomain: true,
-            dataType: 'jsonp',
-            success: function(result) {
-                console.log(result);
-                go = result;
-            },
-            error: function() { alert('Failed!');
-            }
-        });
-        */
-    document.getElementById('allbuttons').innerHTML += "'></button>";
-}
 
 function setSelect() {
     var returnVal = "";
@@ -39,14 +7,61 @@ function setSelect() {
     document.getElementById("numResults").innerHTML += returnVal;
 }
 
-function display(obj) {
+/*
+$(document).ready(
+    $('#artistName').keyup(function(event) {
+        if(event.which === 'enter') {
+            display();
+        }
+    })
+);
+*/
 
-    var table = "<table id = 'displaytable'>";
-    var mani = obj.results;
-    table += '<th>'+ mani.artistName + '</th>';
-    for (var i = 0; i < document.getElementById("numResults").value; i++) {
-        table += '<tr><td>'+mani[i].collectionCensoredName + '</td><td>' +  mani[i].trackName+'</td></tr>';
-    }
-    table += "</table>";
-    document.getElementById("display").innerHTML = table;
+function display() {
+
+    $.ajax({
+        url: 'https://itunes.apple.com/search?term=' + document.getElementById('artistName').value,
+        type: 'GET',
+        crossDomain: true,
+        dataType: 'jsonp',
+        success: function(result) {
+            if(result.results.length < 1) {
+                alert('failed');
+                return 0;
+            }
+            console.log(result);
+            var table = "<table id = 'displaytable'>";
+            var mani = result.results;
+            var ret = [];
+            var usedRet = [];
+            table += '<th>'+ document.getElementById('artistName').value + '</th>';
+
+            for (var i = 0; (i < document.getElementById("numResults").value && i < mani.length); i++) {
+                if(mani[i].kind === 'song') {
+                    ret.push(mani[i]);
+                }
+            }
+            for (var x = 0; x < document.getElementById('numResults').value; x++) {
+
+                for (var z = 0; z < ret.length; z ++) {
+                    if(z>x) {
+                        x=z;
+                    }
+                    if(ret[z] === ret[x]){
+                        ret = ret.slice(z);
+                    }
+                }
+                console.log(ret[x]);
+                table += '<tr><td>' + ret[x].artistName + '</td><td><image src ="'+ret[x].artworkUrl100 +'"></td><td>'+ret[x].collectionName + '</td></tr>';
+
+            }
+
+            table += "</table>";
+            document.getElementById("display").innerHTML = table;
+        },
+        error: function() { alert('Failed!');
+        }
+    });
+
+
 }
