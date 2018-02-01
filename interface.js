d0335da3841da33abef944927f40126ace24af76
Nora Coates <noracoates@students.berkeley.net>
2018-02-01 10:35:20 -0800

    jquery

--- interface.js
+++ interface.js
@@ -35,30 +35,46 @@ function display() {
 
             var table = "<table id = 'displaytable'>";
             var albums = [];
+            var finalArray = [];
             var titles = "";
             table += '<th>'+ document.getElementById('artistName').value + '</th>';
 
-            for (var i = 0; (i < document.getElementById("numResults").value && i < result.results.length); i++) {
+            for (var i = 0; i < result.results.length; i++) {
                 if(result.results[i].kind === 'song') {
                     albums.push(result.results[i]);
                 }
             }
-            for (var x = 0; x < document.getElementById('numResults').value && x < albums.length ; x++) {
+            console.log(albums);
+            for (var x = 0; x < document.getElementById("numResults").value; x++) {
                 titles = albums[x].collectionName;
-                console.log(albums);
-                for (var z = 0; z <= document.getElementById('numResults').value && z < albums.length && albums.length >=1; z ++) {
-                    var thisOne = albums[z].collectionName;
-                    if(titles === thisOne){
-                        //need to add album to table just once
-                        albums = albums.slice(z+1);
-                        console.log(albums);
+                var thisOne = "";
+                for (var z = 0; z < albums.length; z ++) {
+                    console.log(finalArray);
+                    if (finalArray.length !==0 && finalArray[x].collectionName !== undefined) {
+                        thisOne = finalArray[x].collectionName;
+                    } else {
+                        thisOne = titles;
                     }
-                    console.log(thisOne);
-                    console.log(albums[x]);
+                    if(thisOne !== titles && finalArray.length < albums.length){
+                        if(albums[z] !== undefined) {
+                            finalArray.push(albums[z]);
+                            albums = albums.slice(z+1);
+                            console.log(titles, thisOne);
+                            console.log(finalArray);
+                        }
+                    } else {
+                        for(var y = 0; y < finalArray.length; y ++) {
+                            if(finalArray[y].collectionName !== titles) {
+                                finalArray.push(albums[z]);
+                                console.log(titles, thisOne);
+                                console.log(finalArray);
+                            }
+                        }
+                    }
                 }
-                if(albums[x] !== undefined) {
-                    table += '<tr><td>' + albums[x].artistName + '</td><td><image src ="'+albums[x].artworkUrl100+'"></td><td>'+albums[x].collectionName+'</td></tr>';
-                }
+            }
+            for (var f = 0; f < document.getElementById('numResults').value; f++) {
+                table += '<tr><td>' + finalArray[f].artistName + '</td><td><image src ="'+finalArray[f].artworkUrl100+'"></td><td>'+finalArray[f].collectionName+'</td></tr>';
             }
 
             table += "</table>";
